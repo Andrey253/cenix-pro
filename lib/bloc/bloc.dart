80c45cd5c1094e793de33a2596f4bc92441c948a
Andrey253 <Andrey253@yandex.ru>
2022-11-29 23:18:55 +0700

    first commit

---
+++ lib/bloc/bloc.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test_task/bloc/state.dart';
+import 'package:test_task/repository/repository.dart';
+
+import 'event.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  LoginBloc(this.repository) : super(const PhoneInputState());
+
+  final LoginRepository repository;
+  RegExp regex = RegExp(r'^\+\d{11}$');
+
+  @override
+  Stream<LoginState> mapEventToState(
+    LoginEvent event,
+  ) async* {
+    if (event is PhoneChangeEvent) {
+      if (regex.hasMatch(event.phone)) {
+        yield const PhoneInputState();
+      } else {
+        yield const PhoneInputState(error: 'Не вырный формат');
+      }
+    }
+
+    if (event is CheckEnteredCode) {
+      yield const LoginSuccessState(true);
+      try {
+        final token = await repository.checkCode(event.phone, event.code);
+        yield const LoginSuccessState(false);
+      } on Exception catch (e) {
+        yield const PhoneInputState(error: 'Pin is not valid');
+      }
+    }
+    if (event is ReenterPhoneEvent) {
+      yield const PhoneInputState();
+    }
+    if (event is PhoneEnteredEvent) {
+      if (regex.hasMatch(event.phone)) {
+        try {
+          yield const LoginSuccessState(true);
+          await repository.requestSms(event.phone);
+          yield const LoginSuccessState(false);
+          yield SmsRequestedState(event.phone);
+        } on Exception catch (e) {
+          yield const PhoneInputState(error: 'Error geting Sms');
+        }
+      }
+
+      //TODO: describe logic
+    }
+  }
+}
---
+++ lib/bloc/event.dart
@@ -0,0 +1,40 @@
+import 'package:equatable/equatable.dart';
+
+abstract class LoginEvent extends Equatable {}
+
+class PhoneEnteredEvent extends LoginEvent {
+  PhoneEnteredEvent(this.phone);
+
+  final String phone;
+
+  @override
+  List<Object?> get props => [phone];
+}
+class PhoneChangeEvent extends LoginEvent {
+  PhoneChangeEvent(this.phone);
+
+  final String phone;
+
+  @override
+  List<Object?> get props => [phone];
+}
+
+class ResendSmsEvent extends LoginEvent {
+  @override
+  List<Object?> get props => [];
+}
+
+class ReenterPhoneEvent extends LoginEvent {
+  @override
+  List<Object?> get props => [];
+}
+
+class CheckEnteredCode extends LoginEvent {
+  CheckEnteredCode(this.code, this.phone);
+  final String code;
+  // added phone
+  final String phone;
+
+  @override
+  List<Object?> get props => [code];
+}
