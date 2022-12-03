3d26bbebf87969dffa73f60c102875c435f49f17
Andrey253 <Andrey253@yandex.ru>
2022-12-04 02:41:12 +0700

    5

--- lib/bloc/bloc.dart
+++ lib/bloc/bloc.dart
@@ -14,45 +14,62 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
   Stream<LoginState> mapEventToState(
     LoginEvent event,
   ) async* {
-    LoginState _state;
-
     if (event is PhoneChangeEvent) {
-      if (regex.hasMatch(event.phone)) {
-        _state = const PhoneInputState();
-      } else {
-        _state = const PhoneInputState(error: 'Не вырный формат');
-      }
-      if (_state != state) yield _state;
+      phoneChange(repository, state, event);
     } else if (event is CheckEnteredCode) {
-      _state = const LoginSuccessState(true);
-      if (_state != state) yield _state;
-      try {
-        //  final token =
-        await repository.checkCode(event.phone, event.code);
-        _state = const LoginSuccessState(false);
-      } on Exception catch (_) {
-        _state = const PhoneInputState(error: 'Pin is not valid');
-      }
-        if (_state != state) yield _state;
-
+      checkEnteredCode(repository, state, event);
     } else if (event is ReenterPhoneEvent) {
-      _state = const PhoneInputState();
-      if (_state != state) yield _state;
+      reenterPhone(repository, state, event);
     } else if (event is PhoneEnteredEvent) {
-      if (regex.hasMatch(event.phone)) {
-        try {
-          _state = const LoginSuccessState(true);
-          if (_state != state) yield _state;
-          await repository.requestSms(event.phone);
-          const LoginSuccessState(false);
-          _state = SmsRequestedState(event.phone);
-        } on Exception catch (_) {
-          _state = const PhoneInputState(error: 'Error geting Sms');
-        }
-          if (_state != state) yield _state;
-      }
-
-      //TODO: describe logic
+      phoneEntered(repository, state, event);
     }
+  }
+
+  Stream<LoginState> phoneEntered(LoginRepository repository, LoginState state,
+      PhoneEnteredEvent event) async* {
+    if (regex.hasMatch(event.phone)) {
+      LoginState _state;
+      try {
+        _state = const LoginSuccessState(true);
+        if (_state != state) yield _state;
+        await repository.requestSms(event.phone);
+        const LoginSuccessState(false);
+        _state = SmsRequestedState(event.phone);
+      } on Exception catch (_) {
+        _state = const PhoneInputState(error: 'Error geting Sms');
+      }
+      if (_state != state) yield _state;
+    }
+  }
+
+  Stream<LoginState> checkEnteredCode(LoginRepository repository,
+      LoginState state, CheckEnteredCode event) async* {
+    LoginState _state;
+    _state = const LoginSuccessState(true);
+    if (_state != state) yield _state;
+    try {
+      //  final token =
+      await repository.checkCode(event.phone, event.code);
+      _state = const LoginSuccessState(false);
+    } on Exception catch (_) {
+      _state = const PhoneInputState(error: 'Pin is not valid');
+    }
+    if (_state != state) yield _state;
+  }
+
+  Stream<LoginState> phoneChange(LoginRepository repository,
+      LoginState state, PhoneChangeEvent event) async* {
+    LoginState _state;
+    if (regex.hasMatch(event.phone)) {
+      _state = const PhoneInputState();
+    } else {
+      _state = const PhoneInputState(error: 'Не вырный формат');
+    }
+    if (_state != state) yield _state;
+  }
+
+  Stream<LoginState> reenterPhone(LoginRepository repository,
+      LoginState state, ReenterPhoneEvent event) async* {
+    if (state != const PhoneInputState()) yield const PhoneInputState();
   }
 }
